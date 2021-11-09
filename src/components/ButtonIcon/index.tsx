47de966f2e48377aaa3824b74e75901c87f283d8
Pedro Scheid <pedroscheid10@gmail.com>
2021-11-08 22:12:11 -0300

    segundo commit

--- src/components/ButtonIcon/index.tsx
+++ src/components/ButtonIcon/index.tsx
@@ -4,24 +4,29 @@ import {
     Text,
     Image,
     TouchableOpacity,
-    Touchable,
+    TouchableOpacityProps
     } from 'react-native'
 
 import DiscordImg from '../../assets/discord.png'
 import { styles } from './styles'
 
-export function ButtonIcon() {
-    return (
-        <TouchableOpacity 
-            style={styles.container}
-            activeOpacity={0.8}>
-        <View style={styles.iconWrapper} >
-            <Image source={DiscordImg} style={styles.icon}/>
-        </View>
+type Props = TouchableOpacityProps &{
+    title: string;
+}
 
-        <Text style={styles.title} >
-            Entrar com Discord
-        </Text>
-        </TouchableOpacity>
-    )
+export function ButtonIcon({ title, ...rest } : Props){
+  return(
+    <TouchableOpacity 
+      style={styles.container} 
+      {...rest }
+    >
+      <View style={styles.iconWrapper}>
+        <Image source={DiscordImg} style={styles.icon} />
+      </View>
+
+      <Text style={styles.title}>
+        { title }
+      </Text>
+    </TouchableOpacity>
+  );
 }
--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -3,20 +3,22 @@ import {
     Text, 
     View, 
     Image, 
-    StatusBar,
 } from 'react-native';
 
 import { ButtonIcon } from '../../components/ButtonIcon'
 import IllustrationImg from '../../assets/illustration.png'
 import { styles } from './styles'
+import { useNavigation } from '@react-navigation/native';
 
 export function SignIn() {
+    const navigation = useNavigation();
+
+    function handleSignIn(){
+        navigation.navigate('Home')
+    }
+
   return (
     <View style={styles.container}>
-        <StatusBar 
-            barStyle="light-content" 
-            backgroundColor="transparent"
-            translucent/>
         <Image 
         source={IllustrationImg} 
         style={styles.image}
@@ -24,17 +26,21 @@ export function SignIn() {
 
         <View style={styles.content}>
             <Text style={styles.title}>
-                Organize {`\n`}
-                suas jogatinas {`\n`}
-                facilmente
+                Conecte-se {'\n'}
+                e organize suas {'\n'}
+                jogatinas
             </Text>
 
             <Text style={styles.subtitle}>
-                Crie grupos para jogar seus games {`\n`}
+                Crie grupos para jogar seus games {'\n'}
                 favoritos com seus amigos
             </Text>
 
-            <ButtonIcon/>
+            <ButtonIcon
+                title="Entrar com Discord"
+                activeOpacity={0.7}
+                onPress={handleSignIn}
+            />
         </View>
     </View>
   );
