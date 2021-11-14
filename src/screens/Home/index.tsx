b7003990d1560f929ef44afae7d3f389fb411562
Pedro Scheid <pedroscheid10@gmail.com>
2021-11-13 23:43:59 -0300

    terceiro commit

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,15 +1,34 @@
-import React from 'react'
-import { View } from 'react-native'
+import React, { useState, useCallback } from 'react';
+import { View, FlatList } from 'react-native';
 
-import { Profile } from '../../components/Profile'
+import { CategorySelect } from '../../components/CategorySelect';
+import { ButtonAdd } from '../../components/ButtonAdd';
+import { Profile } from '../../components/Profile';
 
-import { styles } from './styles'
+import { styles } from './styles';
 
 export function Home() {
+    const [category, setCategory] = useState('')
+
+    function handleCategorySelect(categoryId: string) {
+        categoryId === category ? setCategory('') : setCategory(categoryId);
+    }
+
     return (
         <View>
             <View style={styles.header}>
                 <Profile />
+                <ButtonAdd />
+            </View>
+
+            <View>
+                <CategorySelect
+                categorySelected={category}
+                setCategory={handleCategorySelect}
+                />
+
+                <View style={styles.matches}>
+                </View>
             </View>
         </View>
     )
